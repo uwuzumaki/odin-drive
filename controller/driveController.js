c5f87c8e3e656f2925272e2dd64232d9e2538997
Kris Wong <krischwong@gmail.com>
2025-05-13 19:15:13 -0400

    Added controllers for registerPost and loginGet

--- controller/driveController.js
+++ controller/driveController.js
@@ -1,12 +1,38 @@
-const index = () => {
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const db = require("../db/queries");
+
+const index = (req, res) => {
   res.render("index");
 };
 
-const registerGet = () => {
+const registerGet = (req, res) => {
   res.render("registerGet");
+};
+
+const registerPost = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await db.newUser(req.body.username, hashedPassword);
+    console.log(user);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const loginGet = (req, res) => {
+  res.render("loginGet");
 };
 
 module.exports = {
   index,
   registerGet,
+  loginGet,
+  registerPost,
 };
