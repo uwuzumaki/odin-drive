580578bcabb79e9eb77f3eaa0a3411a17f922ba2
Kris Wong <krischwong@gmail.com>
2025-05-13 19:14:13 -0400

    added registration and routes for login get and register post

--- router/driveRouter.js
+++ router/driveRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const driveController = require("../controller/driveController");
+const registration = require("../validation/registration");
 
 const driveRouter = Router();
 
 driveRouter.get("/", driveController.index);
 driveRouter.get("/register", driveController.registerGet);
+driveRouter.get("/login", driveController.loginGet);
+driveRouter.post("/register", registration, driveController.registerPost);
 
 module.exports = driveRouter;
